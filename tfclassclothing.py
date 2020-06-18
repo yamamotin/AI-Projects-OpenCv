f306c7d8e880017e050cdac946ab55a1f20353b3
yamamotin <35852852+yamamotin@users.noreply.github.com>
2020-06-18 13:49:13 -0300

    Add files via upload

---
+++ tfclassclothing.py
@@ -0,0 +1,38 @@
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),  # input data
+    keras.layers.Dense(128, activation='relu'),  # hidden layer
+    keras.layers.Dense(10, activation='softmax') # output data
+])
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10)  # epochs rodando 10 vezes
+
+#evaluating the model
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=1)
+
+print('Test accuracy:', test_acc)
+
+#previsao
+predictions = model.predict(test_images)
+
